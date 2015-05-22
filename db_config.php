b79d99ba7ff2ede0e82ca04b0d578189c93dcbda
mboya <mboyaberry@gmail.com>
2015-05-22 17:32:51 +0300

    server credentials

---
+++ db_config.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * All database connection variables
+ */
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "mboya12345"); // db password (mention your db password here)
+define('DB_DATABASE', "haco"); // database name
+define('DB_SERVER', "localhost"); // db server
+?>
